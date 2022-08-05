4cae96984050db70e55af84079b556ac8a41d9b5
SergPaltsev <you@example.com>
2022-08-05 14:11:11 +0300

    created

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 80;
+
+app.get('/', (req, res) => {
+    res.end('<h1>Home page my NodeJS</h1>');
+})
+
+app.listen(PORT, () => {
+    console.log('Server has been started...');
+})

0041639726ce035a1231e97e219ca38203cbd36d
Serg <paltsevserg@gmail.com>
2022-08-14 17:56:59 +0300

    refactoring

--- routes.js
+++ routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const User = require('./models/user');
 const router = express.Router();
 
-// Get all users
 router.get('/users', async (req, res) => {
     const users = await User.find();
     res.send(users);
